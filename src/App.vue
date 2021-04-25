7676fc3eb48ed20cf440ca64ce8374765351f8f5
Ryan Morning <ryanmorning96@gmail.com>
2021-04-25 14:32:35 -0400

    Vue.Js checkout form

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+ <div id="app">
+ <CheckoutPage/>
+ </div>
+</template>
+
+<script>
+import CheckoutPage from "./pages/CheckoutPage.vue"
+export default {
+  name: 'App',
+  components: {
+    CheckoutPage
+  }
+}
+</script>
+
+<style>
+</style>
